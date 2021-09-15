6285f87373bbaea8da76339d56c2e7c558248ace
Alex Alexeyev <aasmc@yandex.ru>
2021-09-15 22:36:36 +0300

    Initial Commit

---
+++ app/src/main/java/ru/aasmc/cryptocurrencies/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.aasmc.cryptocurrencies.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.aasmc.cryptocurrencies.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
